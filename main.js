f8bda959920c0a349178a9409f0644d3e8f56281
MotiurRahmanSany <mdmotiurrahmansany@gmail.com>
2023-10-19 00:37:04 +0600

    Modified

---
+++ main.js
@@ -0,0 +1,12 @@
+// let n = prompt("Insert a number");
+// for (let i = 1; i < n; i++)
+//     console.log(i);
+
+let num = document.querySelector('#time');
+setInterval(showTime, 1000);
+
+function showTime(){
+    let dateTime = new Date();
+    let time = dateTime.toLocaleTimeString();
+    num.innerText = time;
+}
